8b4d74e9e3cca26f9110452999185af5860e49af
alliefeldman <alliefeldman25@gmail.com>
2022-05-08 23:46:18 -0400

    updated

--- README.md
+++ README.md
@@ -4,26 +4,20 @@ Website Link: https://weight-training-interactive.web.app
 
 GitHub Link: https://github.com/alliefeldman/weight-training-interactive
 
-Video Link: https://youtu.be/EJHbUfIqvXU
-
 Description: Right now, the weight rooms in gyms are lacking in diversity. It is one of my initiatives to encourage people of all demographics to not be intimidated to pick up heavy things! This website is an information guide on the benefits of strength training, how to begin, and even allows you to create and view a custom workout program based on your needs. 
 
 
 Using the website, a user can:
 - Read about the benefits of weight training
 	- Scroll down the Information Page
-- Browse some common types of exercises
+- Browse some common types of exercises and workout splits
 	- Scroll through the Information Page
-- Filter exercises to view by muscle group
-	- Click on the filters on the Information Page
 - Design an example of a workout program based on their experience, goals, and available environment.
 	- Fill out the Information on the Create page
 - Contact me via the contact form
 	- On the contact page
 - Find my email addresses and social media accounts
 	- on the contact page
-- Change the color of "interactive"
-	- Hover over the "interactive" on the home page
 
 External libraries: 
 
@@ -35,9 +29,6 @@ jquery:
 Bootstrap:
 - Why: Bootstrap allowed me to create consistent form elements and buttons throughout my website
 - How: The library was used to crate the tabs on my "Create a Program" page
-
-Iteration from HW7:
-I added a lot of interactive features along the way in the creation of this website. HW7 included the general framework for the website, but it wasn't until this assignment where I figured out, for example, how to filter exercises to display on the home page, until after I designed the structure of my other JS functions (I was able to utilize the same dataset for both the information page and the create page!)
 
 
 Major challenges:
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var listOfExercises = {
-"initial":{"firstTwo":false, "lastThree":false,"gym":false, "home":false, "Upper Body":false, "Lower Body":false, "Push":false, "Pull":false, "Legs":false},
+const listOfExercises = {
+"initial": {"firstTwo":false, "lastThree":false,"gym":false, "home":false, "Upper Body":false, "Lower Body":false, "Push":false, "Pull":false, "Legs":false},
 "Barbell Deadlift":{"firstTwo":true, "lastThree":false,"gym":true, "home":false, "Upper Body":false, "Lower Body":true, "Push":false, "Pull":false, "Legs":true},
 "Bench Press":{"firstTwo": true, "lastThree":false,"gym":true, "home": false, "Upper Body":true, "Lower Body":false, "Push":true, "Pull":false, "Legs":false },
 "Back Squat":{"firstTwo": true, "lastThree":false,"gym":true, "home": false, "Upper Body":false, "Lower Body":true,"Push":false, "Pull":false, "Legs":true},
@@ -28,9 +28,45 @@ var listOfExercises = {
 "Dumbbell Overhead Tricep Extension":{"firstTwo": false, "lastThree":true,"gym":true, "home": true, "Upper Body":true, "Lower Body":false, "Push":true, "Pull":false, "Legs":false },
 "Resistance Band Pulldown":{"firstTwo": true, "lastThree":true,"gym":false, "home": true, "Upper Body":true, "Lower Body":false, "Push":false, "Pull":true, "Legs":false },
 "Resistance Band Facepull":{"firstTwo": false, "lastThree":true,"gym":false, "home": true, "Upper Body":true, "Lower Body":false, "Push":false, "Pull":true, "Legs":false },
-"Skull Crusher":{"firstTwo": false, "lastThree":true,"gym":true, "home": true, "Upper Body":true, "Lower Body":false, "Push":true, "Pull":false, "Legs":false }
-
+"Skull Crusher":{"firstTwo": false, "lastThree":true,"gym":true, "home": true, "Upper Body":true, "Lower Body":false, "Push":true, "Pull":false, "Legs":false },
+"Walking Lunges":{"firstTwo": false, "lastThree":true,"gym":true, "home": false, "Upper Body":false, "Lower Body":true, "Push":false, "Pull":false, "Legs":true },
+"Single Leg Glute Bridges":{"firstTwo": false, "lastThree":true,"gym":true, "home": true, "Upper Body":false, "Lower Body":true, "Push":false, "Pull":false, "Legs":true }
 };
+
+const exerciseDetails = {
+    "base":{"you_will_need":[], "steps":[]},
+    "Barbell Deadlift":{"you_will_need":["A barbell rack (supports recommended)", "Barbell", "An equal weight of plates to put on either side", "Barbell clips (optional)"], "steps":[]},
+    "Bench Press":{},
+    "Back Squat":{},
+    "Machine Lat Pulldown":{},
+    "Dumbbell Bicep Curl":{},
+    "Romanian Deadlift":{},
+    "Bulgarian Split Squat":{},
+    "Leg Press":{},
+    "Goblet Squat":{},
+    "Barbell Shoulder Press":{},
+    "Dumbbell Shoulder Press":{},
+    "Lateral Raises":{},
+    "Hip Abductions":{},
+    "Glute Bridges": {},
+    "Hip Thrust":{},
+    "Leg Curl":{},
+    "Leg Extension":{},
+    "Seated Row":{},
+    "Bent Over Row":{},
+    "Hammer Curls":{},
+    "Pushups":{},
+    "Dumbbell Chest Press":{},
+    "Single Arm Rows":{},
+    "Pull Aparts (resistance band)":{},
+    "Dumbbell Calf Raises":{},
+    "Dumbbell Overhead Tricep Extension":{},
+    "Resistance Band Pulldown":{},
+    "Resistance Band Facepull":{},
+    "Skull Crusher":{},
+    "Walking Lunges":{},
+    "Single Leg Glute Bridges":{}
+}
 
 // from https://stackoverflow.com/questions/2532218/pick-random-property-from-a-javascript-object
 function pickRandomExercise(obj) {
@@ -42,34 +78,91 @@ function pickRandomExercise(obj) {
     return result;
 }
 
+var scrollY = 0;
+var what_is_strength_training_y;
+var goals_y;
+var freq_y;
+var envir_y;
+var selection_y;
+
+function getElemCoordinates() {
+    let what_is_strength_training = document.getElementById("what_is_strength_training");
+    what_is_strength_training_y = what_is_strength_training.getBoundingClientRect().y;
+
+    let goals = document.getElementById("goals");
+    goals_y = goals.getBoundingClientRect().y;
+
+    let freq = document.getElementById("frequency");
+    freq_y = freq.getBoundingClientRect().y;
+
+    let envir = document.getElementById("environment");
+    envir_y = envir.getBoundingClientRect().y;
+
+    let selection = document.getElementById("exercise_selection");
+    selection_y = selection.getBoundingClientRect().y;
+}
+
+function scroll() {
+    scrollY = window.pageYOffset;
+    let indicator_image = document.getElementById("fixed_indicator");
+    if (scrollY < (what_is_strength_training_y - 200)) {
+        indicator_image.setAttribute("src", "fixed_indicator/5.png");
+    }
+    else if (scrollY < (goals_y - 150)) {
+        indicator_image.setAttribute("src", "fixed_indicator/0.png");
+    }
+    else if (scrollY < (freq_y - 150)) {
+        indicator_image.setAttribute("src", "fixed_indicator/1.png");
+    }
+    else if (scrollY < (envir_y - 150)) {
+        indicator_image.setAttribute("src", "fixed_indicator/2.png");
+    }
+    else if (scrollY < (selection_y - 150)) {
+        indicator_image.setAttribute("src", "fixed_indicator/3.png");
+    }
+    else {
+        indicator_image.setAttribute("src", "fixed_indicator/4.png");
+    }
+}
+
+
 function scroll_to_id(id){
     let scroll_to_element = document.getElementById(id);
-    console.log(id);
     scroll_to_element.scrollIntoView();
 }
 
 
 function addDescription(id) {
-    console.log(id);
     let expanded_div = document.getElementById("expanded");
     expanded_div.innerHTML = "";
-    console.log(expanded_div.innerHTML);
-    description_div = document.createElement("div");
+    let button = document.getElementById(id);
+    if (button.classList[0] == "selected_goal") {
+        button.classList.remove("selected_goal");
+        return;
+    }
+    try {
+        let currentlySelected = document.getElementsByClassName("selected_goal")[0];
+        currentlySelected.classList.remove("selected_goal");
+    }
+    catch {
+
+    }
+    button.classList.add("selected_goal");
+    let description_div = document.createElement("div");
     let description = document.createElement("p");
     description.setAttribute("id", "description");
     if (id == "strength") {
-        description.innerText = "Tristique senectus et netus et. Neque sodales ut etiam sit. Non quam lacus suspendisse faucibus. Nec ullamcorper sit amet risus nullam eget. Nisl rhoncus mattis rhoncus urna neque viverra justo nec ultrices. Nunc sed augue lacus viverra vitae. Eget mauris pharetra et ultrices neque ornare aenean euismod elementum. Est ullamcorper eget nulla facilisi etiam dignissim diam quis enim.";
+        description.innerText = "Tristique senectus et netus et. Neque sodales ut etiam sit. Non quam lacus suspendisse faucibus. Nec ullamcorper sit amet risus nullam eget. Nisl rhoncus mattis rhoncus urna neque viverra justo nec ultrices. Nunc sed augue lacus viverra vitae. Eget mauris pharetra et ultrices neque ornare. \n\n     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada bibendum arcu vitae elementum curabitur. Quis blandit turpis cursus in hac habitasse platea dictumst. Sed augue lacus viverra vitae congue eu consequat ac. Et malesuada fames ac turpis egestas integer eget. Sed adipiscing diam donec adipiscing tristique risus. Vitae congue mauris rhoncus aenean vel elit scelerisque. In massa tempor nec feugiat nisl pretium fusce id. Arcu bibendum at varius vel pharetra. Leo in vitae turpis massa sed elementum. In fermentum posuere urna nec. Lorem donec massa sapien faucibus et.\n\nIn fermentum posuere urna nec tincidunt. Non consectetur a erat nam at lectus. Aliquam eleifend mi in nulla. Ornare massa eget egestas purus viverra. Odio ut sem nulla pharetra diam sit amet nisl suscipit. Volutpat ac tincidunt vitae semper quis. Nulla malesuada pellentesque elit eget gravida cum sociis. Fermentum et sollicitudin ac orci phasellus egestas tellus. At volutpat diam ut venenatis. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. Duis at tellus at urna.";
     }
     else if (id == "mental") {
-        description.innerText = "Ultricies mi quis hendrerit dolor magna. Pellentesque elit eget gravida cum sociis natoque penatibus et magnis. Magna fermentum iaculis eu non diam phasellus. Imperdiet sed euismod nisi porta lorem mollis. Massa vitae tortor condimentum lacinia quis vel. Vitae purus faucibus ornare suspendisse sed. Volutpat commodo sed egestas egestas fringilla phasellus. Non enim praesent elementum facilisis leo vel fringilla. Massa tempor nec feugiat nisl pretium.";
+        description.innerText = "Ultricies mi quis hendrerit dolor magna. Imperdiet sed euismod nisi porta lorem mollis. Massa vitae tortor condimentum lacinia quis vel. Vitae purus faucibus ornare suspendisse sed. Volutpat commodo sed egestas egestas fringilla phasellus. Non enim praesent elementum facilisis leo vel fringilla. Massa tempor nec feugiat nisl pretium.Massa enim nec dui nunc. Sit amet massa vitae tortor. Lobortis mattis aliquam faucibus purus in massa tempor. Risus nullam eget felis eget nunc lobortis. Morbi tincidunt ornare massa eget egestas purus viverra. Quisque non tellus orci ac auctor. Non diam phasellus vestibulum lorem sed. Pulvinar sapien et ligula ullamcorper malesuada. Commodo elit at imperdiet dui accumsan sit amet nulla facilisi. Tempor nec feugiat nisl pretium fusce id velit. Risus in hendrerit gravida rutrum quisque non tellus orci ac. Viverra nibh cras pulvinar mattis nunc. Non nisi est sit amet facilisis magna etiam. Quis risus sed vulputate odio. Mauris pharetra et ultrices neque ornare aenean euismod elementum. Urna cursus eget nunc scelerisque viverra mauris in. Feugiat pretium nibh ipsum consequat. Sit amet est placerat in egestas erat. Consequat id porta nibh venenatis.Aenean euismod elementum. Est ullamcorper eget nulla facilisi etiam dignissim diam quis enim.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies integer quis. Lacus vestibulum sed arcu non odio euismod lacinia. Amet consectetur adipiscing elit duis tristique.";
     }
     else if (id == "aesthetics") {
-        description.innerText = "Molestie a iaculis at erat pellentesque adipiscing. Accumsan sit amet nulla facilisi morbi. Sollicitudin aliquam ultrices sagittis orci a scelerisque. Magna sit amet purus gravida quis blandit. Sagittis purus sit amet volutpat consequat. Nec dui nunc mattis enim. Fusce id velit ut tortor pretium viverra suspendisse. Urna duis convallis convallis tellus. Enim neque volutpat ac tincidunt vitae semper quis. Dui sapien eget mi proin sed libero. Viverra orci sagittis eu volutpat odio. Lorem ipsum dolor sit amet consectetur adipiscing elit.";
+        description.innerText = "Molestie a iaculis at erat pellentesque adipiscing. Accumsan sit amet nulla facilisi morbi. Sollicitudin aliquam ultrices sagittis orci a scelerisque. Magna sit amet purus gravida quis blandit. Sagittis purus sit amet volutpat consequat. Nec dui nunc mattis enim. Fusce id velit ut tortor pretium viverra suspendisse. Urna duis convallis convallis tellus. Enim neque volutpat ac tincidunt vitae semper quis. Dui sapien eget mi proin sed libero. Viverra orci sagittis eu volutpat odio. Pulvinar neque laoreet suspendisse interdum consectetur. Sagittis aliquam malesuada bibendum arcu vitae elementum. Orci ac auctor augue mauris augue neque gravida in fermentum. Volutpat blandit aliquam etiam erat velit scelerisque. Mauris sit amet massa vitae. Egestas congue quisque egestas diam. Non pulvinar neque laoreet suspendisse interdum consectetur libero id. Donec ultrices tincidunt arcu non sodales neque. Pellentesque elit ullamcorper dignissim cras tincidunt lobortis. Quam vulputate dignissim suspendisse in est ante in nibh. Turpis tincidunt id aliquet risus feugiat. Leo vel fringilla est ullamcorper eget nulla facilisi etiam. Vitae purus faucibus ornare suspendisse sed. Tincidunt arcu non sodales neque sodales ut etiam sit amet. Enim ut sem viverra aliquet eget sit amet tellus cras. Tempus urna et pharetra pharetra massa. Nibh ipsum consequat nisl vel pretium. Augue neque gravida in fermentum et. Velit aliquet sagittis id consectetur purus ut. Lorem ipsum dolor sit amet consectetur adipiscing elit.";
     }
     description_div.appendChild(description);
     expanded_div.appendChild(description_div);
 }
-
 
 
 function selectExperience(element) {
@@ -97,7 +190,8 @@ function selectFrequency(item_class, id) {
     selected_element.classList.add("selected_frequency");
     // then, addd a new text element
     let box = document.getElementById("frequency_description");
-    box.innerText = `${id}: Tristique senectus et netus et. Dolor morbi non arcu risus quis varius quam quisque id. At erat pellentesque adipiscing commodo. Fermentum iaculis eu non diam phasellus vestibulum lorem sed. Malesuada nunc vel risus commodo. Neque sodales ut etiam sit. Non quam lacus suspendisse faucibus. Nec ullamcorper sit amet risus nullam eget. Nisl rhoncus mattis rhoncus urna neque viverra justo nec ultrices. Nunc sed augue .`
+    box.innerText = "";
+    box.innerText = `${id}: Tristique senectus et netus et. Dolor morbi non arcu risus quis varius quam quisque id. At erat pellentesque adipiscing commodo. Fermentum iaculis eu non diam phasellus vestibulum lorem sed. Malesuada nunc vel risus commodo. Neque sodales ut etiam sit. Non quam lacus suspendisse faucibus. Nec ullamcorper sit amet risus nullam eget. Nisl rhoncus mattis rhoncus urna neque viverra justo nec ultrices. Nunc sed augue .`;
 
 }
 
@@ -146,7 +240,6 @@ function update_push_pull(className) {
     else {
         push_pull_filter = className;
     }
-    console.log(push_pull_filter);
 }
 
 function selectUpperOrLower(element){
@@ -199,13 +292,14 @@ function display_push_pull_buttons() {
     pullbutton.setAttribute("onclick", "update_push_pull(this.classList.toString()); selectPushOrPull(this); displayExercises()");
     pullbutton.innerText = "Pull";
 
-
+    buttons_div.appendChild(document.createElement("hr"));
     buttons_div.appendChild(pushbutton);
     buttons_div.appendChild(pullbutton);
     all_buttons.appendChild(buttons_div);
 }
 
 function displayExercises() {
+
     let exercisesDiv = document.getElementById("exercises");
     exercisesDiv.innerHTML = "";
     for (exercise in listOfExercises) {
@@ -227,8 +321,10 @@ function displayExercises() {
             }
 
         }
-        exercisesDiv.appendChild(exercise_h4)
+        exercise_h4.setAttribute("onclick", `showExerciseInfo("${exercise}")`);
+        exercisesDiv.appendChild(exercise_h4);
     }
+
 
 }
 
@@ -319,7 +415,6 @@ function createProgram() {
     else {
         environment = "home";
     }
-    console.log(environment);
 
 
 
@@ -332,7 +427,6 @@ function createProgram() {
 
     for(let day = 0; day < frequency; day++) {
         let workout = split[day];
-        console.log(split);
         let dayDiv = document.createElement("div");
         let dayHeader = document.createElement("h2");
         dayHeader.innerText = `Day ${day+1}/${frequency}: ${split[day]}`;
@@ -360,4 +454,57 @@ function createProgram() {
     }
 }
 
+function showExerciseInfo(exercise) {
+    let body = document.body;
+    let exerciseInfoClass = document.createElement("div");
+    exerciseInfoClass.setAttribute("class", "exercise_info_div");
 
+    console.log(exercise);
+
+    let x = document.createElement("button");
+    x.innerText = "X";
+    x.setAttribute("onclick", "hideExerciseInfo()");
+
+    let exercise_info_inner = document.createElement("div");
+    exercise_info_inner.setAttribute("id", "exercise_info_inner");
+
+    let exercise_heading = document.createElement("h1");
+    exercise_heading.innerText = exercise;
+    exercise_info_inner.appendChild(exercise_heading);
+
+    let you_will_need_heading = document.createElement("h2");
+    you_will_need_heading.innerText = "You will need:";
+    exercise_info_inner.appendChild(you_will_need_heading);
+    let ul = document.createElement("ul");
+    for (var item = 0; item < exerciseDetails[exercise]["you_will_need"].length; item++) {
+        let li = document.createElement("li");
+        li.innerText = exerciseDetails[exercise]["you_will_need"][item];
+        ul.appendChild(li);
+    }
+    exercise_info_inner.appendChild(ul);
+
+    let steps_heading = document.createElement("h2");
+    steps_heading.innerText = "Steps:";
+    exercise_info_inner.appendChild(steps_heading);
+
+    let ol = document.createElement("ol");
+    for (var item = 0; item < exerciseDetails[exercise]["steps"].length; item++) {
+        let li = document.createElement("li");
+        li.innerText = exerciseDetails[exercise]["steps"][item];
+    }
+
+
+
+    exerciseInfoClass.appendChild(x);
+    exerciseInfoClass.appendChild(exercise_info_inner);
+
+
+    body.appendChild(exerciseInfoClass);
+}
+
+
+function hideExerciseInfo() {
+    let body = document.body;
+    let exerciseInfoClass = document.getElementsByClassName("exercise_info_div")[0];
+    body.removeChild(exerciseInfoClass);
+}
